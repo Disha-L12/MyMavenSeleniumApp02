413cda57e85467090ef2539e095cfa5087c7439d
Disha L <disha1222005@gmail.com>
2026-04-16 15:43:32 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -3,25 +3,32 @@ package com.example;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
-/**
- * Hello world!
- *
- */
-public class App 
+public class App
 {
-    public static void main( String[] args )
+    public static void main(String[] args)
     {
-        WebDriver driver=new ChromeDriver();
-        options.addArguments("--headless=new");       // modern headless mode
-        options.addArguments("--no-sandbox");         // required in Jenkins/Linux
-        options.addArguments("--disable-dev-shm-usage"); // prevents crashes
+        ChromeOptions options = new ChromeOptions();
+
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--disable-gpu");
-        options.addArguments("--window-size=1920,1080"); // optional but useful
+        options.addArguments("--window-size=1920,1080");
+
+        WebDriver driver = new ChromeDriver(options);
+
         driver.get("https://www.saucedemo.com/");
         driver.manage().window().maximize();
+
         driver.findElement(By.id("user-name")).sendKeys("standard_user");
         driver.findElement(By.id("password")).sendKeys("secret_sauce");
         driver.findElement(By.id("login-button")).click();
+
+        System.out.println("Login Successful");
+        System.out.println(driver.getCurrentUrl());
+
+        driver.quit();
     }
 }
